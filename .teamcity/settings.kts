5f57846d49ec3ba266d80b6b4dfac9fddad490c0
liveperson <liveperson@TeamCity>
2018-10-25 23:20:21 +0000

    TeamCity change in 'Android / CI Example Android' project: Versioned settings configuration updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -28,6 +28,47 @@ version = "2018.1"
 project {
 
     buildType(Build)
+
+    features {
+        feature {
+            id = "PROJECT_EXT_5"
+            type = "CloudImage"
+            param("imageId", "jetbrains/teamcity-agent")
+            param("memory", "4")
+            param("numberCores", "2")
+            param("groupId", "teamcity")
+            param("agent_pool_id", "-2")
+            param("storageAccount", "teamcity7a69e190cf")
+            param("vmUsername", "")
+            param("reuseVm", "false")
+            param("source-id", "teamcity-agent")
+            param("deployTarget", "SpecificGroup")
+            param("vmPublicIp", "false")
+            param("profileId", "arm-2")
+            param("imageUrl", "")
+            param("osType", "Linux")
+            param("maxInstances", "2")
+            param("region", "westus")
+            param("imageType", "Container")
+        }
+        feature {
+            id = "arm-2"
+            type = "CloudProfile"
+            param("profileServerUrl", "")
+            param("total-work-time", "")
+            param("credentialsType", "msi")
+            param("description", "")
+            param("cloud-code", "arm")
+            param("enabled", "true")
+            param("environment", "AZURE")
+            param("agentPushPreset", "1540497845915")
+            param("profileId", "arm-2")
+            param("name", "Azure Cloud")
+            param("subscriptionId", "8b53b792-8973-4a30-a63e-4d0e9ff9e3f5")
+            param("terminate-idle-time", "30")
+            param("secure:passwords_data", "credentialsJSON:e792586a-6c9a-4bbd-8e31-3f014dfd2535")
+        }
+    }
 }
 
 object Build : BuildType({
