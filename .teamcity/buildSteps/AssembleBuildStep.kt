1ff0bddef03f93757f37b69fa04bbb60e9b98b3a
Randy Webster <randy.webster@globant.com>
2018-10-31 15:50:30 -0700

    Update gradle wrapper path

--- .teamcity/buildSteps/AssembleBuildStep.kt
+++ .teamcity/buildSteps/AssembleBuildStep.kt
@@ -4,6 +4,6 @@ import jetbrains.buildServer.configs.kotlin.v2018_1.buildSteps.GradleBuildStep
 
 class AssembleBuildStep : GradleBuildStep({
     dockerImage = "randr0id/android-docker"
-    gradleWrapperPath = "gradlew"
+    gradleWrapperPath = "./gradlew"
     tasks = "assembleRelease"
 })
--- .teamcity/buildSteps/LintTestBuildStep.kt
+++ .teamcity/buildSteps/LintTestBuildStep.kt
@@ -4,6 +4,6 @@ import jetbrains.buildServer.configs.kotlin.v2018_1.buildSteps.GradleBuildStep
 
 class LintTestBuildStep : GradleBuildStep({
     dockerImage = "randr0id/android-docker"
-    gradleWrapperPath = "gradlew"
+    gradleWrapperPath = "./gradlew"
     tasks = "lintRelease"
 })
